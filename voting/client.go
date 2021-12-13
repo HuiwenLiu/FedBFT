e764874d0762ac82661f97d4a569c6ee378d4fb6
Retriever <16319106+SheldonHH@users.noreply.github.com>
2021-12-13 20:54:05 +0800

    rename

--- voting/client.go
+++ voting/client.go
@@ -5,16 +5,18 @@ import (
 	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/big"
 	"os"
 	"strings"
 	"time"
-	"io/ioutil"
 )
+
 // This is the 1024-bit MODP group from RFC 5114, section 2.1:
 const primeHex = "B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371"
 const generatorHex = "A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5"
+
 var encryptedData string
 
 func clientSendMessageAndListen() {
@@ -45,8 +47,6 @@ func clientSendMessageAndListen() {
 		}
 		fmt.Println("VALUES SLICE：", values)
 
-
-
 		if err != nil {
 			fmt.Println("Error reading from stdin")
 			panic(err)
@@ -66,7 +66,7 @@ func clientSendMessageAndListen() {
 
 		fmt.Println("c1: ", c1)
 		fmt.Println("c2: ", c2)
-		encryptedData := c1.String()+"+"+c2.String()
+		encryptedData := c1.String() + "+" + c2.String()
 		fmt.Println("c1+c2: ", encryptedData)
 		//t1 := big.NewInt(c1)
 		//t2 := big.NewInt(c2)
@@ -104,7 +104,6 @@ func getRandom() int {
 	}
 }
 
-
 func fromHex(hex string) *big.Int {
 	n, ok := new(big.Int).SetString(hex, 16)
 	if !ok {
@@ -112,4 +111,3 @@ func fromHex(hex string) *big.Int {
 	}
 	return n
 }
-
